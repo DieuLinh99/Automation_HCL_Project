c040876885a68ff4524661c37909083b73d90648
legallolicon <truonghoanglong2507@gmail.com>
2021-10-29 16:50:39 +0700

    update

--- src/ClientRegPOM/Registration.java
+++ src/ClientRegPOM/Registration.java
@@ -1,8 +1,16 @@
+package testng_hcl;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
-public class Form2 {
+public class Registration {
     private WebDriver driver;
+    By regButton = By.linkText("Register");
+    By clientButton = By.xpath("//div[text()='A Client']");
+    By userText = By.xpath("//input[@formcontrolname='userId']");
+    By passwordText = By.xpath("//input[@formcontrolname='password']");
+    By confirmPasswordText = By.xpath("//input[@formcontrolname='confirm_password']");
+
     By firstNameText = By.xpath("//input[@formcontrolname='firstName']");
     By lastNameText = By.xpath("//input[@formcontrolname='lastName']");
     By emailText = By.xpath("//input[@formcontrolname='email']");
@@ -14,8 +22,32 @@ public class Form2 {
     By countryText = By.xpath("//input[@formcontrolname='country']");
     By nextButton = By.xpath("//span[text()='Next']");
 
-    public Form2(WebDriver driver) {
+    By accountText = By.xpath("//input[@formcontrolname='accountNumber']");
+    By verificationButton = By.xpath("//span[text()='Get Verification Code']");
+
+    public Registration(WebDriver driver) {
         this.driver = driver;
+    }
+
+
+    public void setRegButton() {
+        driver.findElement(this.regButton).click();
+    }
+
+    public void setClientButton() {
+        driver.findElement(this.clientButton).click();
+    }
+
+    public void setUserText(String userText) {
+        driver.findElement(this.userText).sendKeys(userText);
+    }
+
+    public void setPasswordText(String passwordText) {
+        driver.findElement(this.passwordText).sendKeys(passwordText);
+    }
+
+    public void setConfirmPasswordText(String confirmPasswordText) {
+        driver.findElement(this.confirmPasswordText).sendKeys(confirmPasswordText + Keys.ENTER);
     }
 
     public void setFirstNameText(String firstNameText) {
@@ -57,6 +89,23 @@ public class Form2 {
     public void setNextButton() {
         driver.findElement(this.nextButton).click();
     }
+
+    public void setAccountText(String accountText) {
+        driver.findElement(this.accountText).sendKeys(accountText);
+    }
+
+    public void setVerificationButton(){
+        driver.findElement(this.verificationButton).click();
+    }
+
+    public void fistForm(String userText, String passwordText, String confirmPasswordText){
+        this.setRegButton();
+        this.setClientButton();
+        this.setUserText(userText);
+        this.setPasswordText(passwordText);
+        this.setConfirmPasswordText(confirmPasswordText);
+    }
+
     public void secondForm(String firstNameText, String lastNameText, String emailText, String phoneText, String addressText, String cityText, String stateText, String provinceText, String countryText){
         this.setFirstNameText(firstNameText);
         this.setLastNameText(lastNameText);
@@ -68,5 +117,9 @@ public class Form2 {
         this.setProvinceText(provinceText);
         this.setCountryText(countryText);
         this.setNextButton();
+    }
+    public void thirdForm(String accountText){
+        this.setAccountText(accountText);
+        this.setVerificationButton();
     }
 }
